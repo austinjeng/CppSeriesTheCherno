7881c81304065e2cfae39a37b15199328fd60701
Wei-Han Jeng <austin60616@gmail.com>
2021-10-31 17:58:04 +0800

    finished how to write class in CPP

--- HelloWorld/HelloWorld/src/Main.cpp
+++ HelloWorld/HelloWorld/src/Main.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
 
-void Log(const char* message);
+class Log
+{
+public:
+	const int LogLevelError = 0;
+	const int LogLevelWarning = 1;
+	const int LogLevelInfo = 2;
+private:
+	// m_ means member variables
+	int m_LogLevel = LogLevelInfo;
+public:
+	void SetLevel(int level)
+	{
+		m_LogLevel = level;
+	}
+
+	void Error(const char* message)
+	{
+		if (m_LogLevel >= LogLevelError)
+			std::cout << "[ERROR]:" << message << std::endl;
+	}
+
+	void Warn(const char* message)
+	{
+		if (m_LogLevel >= LogLevelWarning)		
+			std::cout << "[WARNING]:" << message << std::endl;
+	}
+
+	void Info(const char* message)
+	{
+		if (m_LogLevel >= LogLevelInfo)		
+			std::cout << "[INFO]:" << message << std::endl;
+	}
+};
+
 
 int main()
 {
-	Log("Hello World");
+	Log log;
+	log.SetLevel(log.LogLevelWarning);
+	log.Warn("Hello!");
+	log.Error("Hello!");
+	log.Info("Geelowow!");
 	std::cin.get();
 }
